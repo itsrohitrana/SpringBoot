c62ebe395b0b1aa6ab346e8c7e2e4b4d01af2d8a
Harsh Kumar 1 <harsh.kumar1@mattsenkumar.com>
2019-10-03 14:13:06 +0530

    remove imoprt

--- src/main/java/com/spring/start/z_startup/App.java
+++ src/main/java/com/spring/start/z_startup/App.java
@@ -2,17 +2,18 @@ package com.spring.start.z_startup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-
-@ComponentScan
+/**
+ * main class who is responsible for the spring loading life cycle
+ * 
+ * @author harsh.kumar1
+ *
+ */
 @SpringBootApplication
-public class App 
-{
-    public static void main( String[] args )
-    {
-        SpringApplication.run(App.class, args);
-       
-    }
-    
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+
+	}
+
 }
